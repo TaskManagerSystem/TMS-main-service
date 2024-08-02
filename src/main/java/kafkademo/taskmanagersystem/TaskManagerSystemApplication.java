9c46fe340fd7703ddde86df039806db5a9b8e069
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-08-02 13:34:28 +0300

    added check style and ci

---
+++ src/main/java/kafkademo/taskmanagersystem/TaskManagerSystemApplication.java
@@ -0,0 +1,13 @@
+package kafkademo.taskmanagersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kafkademo/taskmanagersystem/TaskManagerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package kafkademo.taskmanagersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
