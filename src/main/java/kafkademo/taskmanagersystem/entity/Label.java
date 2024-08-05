d5c5c2bb909438dc92b5bfcd8a468e0287d8afe9
Oleksandr Farion <sashafarion@gmail.com>
2024-08-05 15:38:22 +0300

    Added label entity (#4)
    * add entity
    
    * add changelog

---
+++ src/main/java/kafkademo/taskmanagersystem/entity/Label.java
@@ -0,0 +1,24 @@
+package kafkademo.taskmanagersystem.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "labels")
+public class Label {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String color;
+}
