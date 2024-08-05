bac333dde24d979d00de07a792d44967cda6bf82
Oleksandr Farion <sashafarion@gmail.com>
2024-08-05 16:27:17 +0300

    Added comment entity (#6)
    * add entity
    
    * add changelog

---
+++ src/main/java/kafkademo/taskmanagersystem/entity/Comment.java
@@ -0,0 +1,30 @@
+package kafkademo.taskmanagersystem.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@Setter
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Task task;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+    private String text;
+    @CreationTimestamp
+    private LocalDateTime timestamp;
+}
