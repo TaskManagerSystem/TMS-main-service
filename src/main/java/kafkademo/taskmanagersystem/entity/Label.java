09975f7cf00d8337e1e6f7dd2c68ed7b827f85d1
reamfox <sashafarion@gmail.com>
2024-08-05 15:34:27 +0300

    fix id generation strategy

--- src/main/java/kafkademo/taskmanagersystem/entity/Label.java
+++ src/main/java/kafkademo/taskmanagersystem/entity/Label.java
@@ -15,7 +15,7 @@ import lombok.Setter;
 @Table(name = "labels")
 public class Label {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
     private String name;
