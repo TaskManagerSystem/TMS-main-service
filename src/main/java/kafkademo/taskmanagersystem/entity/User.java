a80066d0327f7cc683e140c2295ad81866edcdd8
DmytroHadiuchko <153805566+DmytroHadiuchko@users.noreply.github.com>
2024-08-05 16:02:50 +0300

    fix: deleted data annotation from project, user entities (#8)
    Co-authored-by: Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>

--- src/main/java/kafkademo/taskmanagersystem/entity/User.java
+++ src/main/java/kafkademo/taskmanagersystem/entity/User.java
@@ -6,13 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Table(name = "users")
-@Data
+@Getter
+@Setter
 @SQLDelete(sql = "UPDATE users SET is_deleted = true WHERE id=?")
 @SQLRestriction("is_deleted=false")
 public class User {
