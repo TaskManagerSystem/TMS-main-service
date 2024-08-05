e8884f14d18202e9de44bf94aeba101323a9038a
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-08-05 13:22:17 +0300

    feature: added liquibase and change log for users table

--- src/main/java/kafkademo/taskmanagersystem/entity/User.java
+++ src/main/java/kafkademo/taskmanagersystem/entity/User.java
@@ -1,5 +1,6 @@
 package kafkademo.taskmanagersystem.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,10 +24,6 @@ public class User {
     private String email;
     private String firstName;
     private String lastName;
-    private Role role;
-}
-
-enum Role {
-    ADMIN,
-    USER;
+    @Column(name = "is_deleted")
+    private boolean isDeleted;
 }
