6ca8a99facacb90693eb94def17521fd94dba3b8
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-08-05 15:54:38 +0300

    fix: deleted data annotation from project, user entities

--- src/main/java/kafkademo/taskmanagersystem/entity/Project.java
+++ src/main/java/kafkademo/taskmanagersystem/entity/Project.java
@@ -9,11 +9,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "projects")
-@Data
+@Getter
+@Setter
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
