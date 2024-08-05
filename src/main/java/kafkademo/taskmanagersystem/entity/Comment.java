dbd13e369a9604aad15f5ec49d5fc7c706bc9e6d
reamfox <sashafarion@gmail.com>
2024-08-05 15:42:00 +0300

    optimize imports

--- src/main/java/kafkademo/taskmanagersystem/entity/Comment.java
+++ src/main/java/kafkademo/taskmanagersystem/entity/Comment.java
@@ -4,11 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "comments")
