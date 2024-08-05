963d1b9daad3d680e59fecb953dfc53035ad205f
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-08-05 15:28:27 +0300

    feature: added task entity and change log

---
+++ src/main/java/kafkademo/taskmanagersystem/entity/Task.java
@@ -0,0 +1,45 @@
+package kafkademo.taskmanagersystem.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+    @Enumerated(EnumType.STRING)
+    private Project.Status status;
+    @Column(name = "due_date")
+    private LocalDate dueDate;
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project projectId;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User userId;
+
+    enum Priority {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+}
+
