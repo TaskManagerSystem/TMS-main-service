b5706e3f3ceff2b8784113f9196a5133effadc5e
reamfox <sashafarion@gmail.com>
2024-08-06 11:48:37 +0300

    remove annotation

--- src/main/java/kafkademo/taskmanagersystem/repo/ProjectRepository.java
+++ src/main/java/kafkademo/taskmanagersystem/repo/ProjectRepository.java
@@ -2,8 +2,6 @@ package kafkademo.taskmanagersystem.repo;
 
 import kafkademo.taskmanagersystem.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 }
