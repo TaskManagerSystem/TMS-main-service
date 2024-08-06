7dbacc16ecbda20a9cd9af805d75998b17b8f8ca
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-08-06 14:22:08 +0300

    fix: fix checkstyle

--- src/main/java/kafkademo/taskmanagersystem/controller/LabelController.java
+++ src/main/java/kafkademo/taskmanagersystem/controller/LabelController.java
@@ -2,6 +2,7 @@ package kafkademo.taskmanagersystem.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
+import java.util.List;
 import kafkademo.taskmanagersystem.dto.label.CreateLabelRequestDto;
 import kafkademo.taskmanagersystem.dto.label.LabelDto;
 import kafkademo.taskmanagersystem.service.LabelService;
@@ -16,8 +17,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/labels")
