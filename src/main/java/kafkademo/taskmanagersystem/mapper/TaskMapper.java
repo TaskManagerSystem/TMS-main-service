6309cbfc956de80917e6a6bef4bd3a7bceee3ba2
reamfox <sashafarion@gmail.com>
2024-08-06 17:18:49 +0300

    fix mapper

--- src/main/java/kafkademo/taskmanagersystem/mapper/TaskMapper.java
+++ src/main/java/kafkademo/taskmanagersystem/mapper/TaskMapper.java
@@ -13,7 +13,7 @@ public interface TaskMapper {
     @Mapping(source = "project.id", target = "projectId")
     TaskDto toDto(Task task);
 
-    @Mapping(target = "projectId", ignore = true)
-    @Mapping(target = "userId", ignore = true)
+    @Mapping(target = "project", ignore = true)
+    @Mapping(target = "user", ignore = true)
     Task toModel(CreateTaskDto createTaskDto);
 }
