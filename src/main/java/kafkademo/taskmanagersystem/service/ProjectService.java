189834ebee44c16b7594ebcfb27c6344d56be5d3
reamfox <sashafarion@gmail.com>
2024-08-06 10:50:28 +0300

    added service structure

--- src/main/java/kafkademo/taskmanagersystem/service/ProjectService.java
+++ src/main/java/kafkademo/taskmanagersystem/service/ProjectService.java
@@ -1,5 +1,18 @@
 package kafkademo.taskmanagersystem.service;
 
+import java.util.List;
+import kafkademo.taskmanagersystem.dto.project.CreateProjectDto;
+import kafkademo.taskmanagersystem.dto.project.ProjectDto;
+
 public interface ProjectService {
 
+    ProjectDto create(CreateProjectDto createProjectDto);
+
+    List<ProjectDto> getByUser(Long userId);
+
+    ProjectDto getById(Long id);
+
+    ProjectDto updateById(Long id);
+
+    ProjectDto deleteById(Long id);
 }
--- src/main/java/kafkademo/taskmanagersystem/service/impl/ProjectServiceImpl.java
+++ src/main/java/kafkademo/taskmanagersystem/service/impl/ProjectServiceImpl.java
@@ -1,4 +1,45 @@
 package kafkademo.taskmanagersystem.service.impl;
 
-public class ProjectServiceImpl {
+import java.util.List;
+import kafkademo.taskmanagersystem.dto.project.CreateProjectDto;
+import kafkademo.taskmanagersystem.dto.project.ProjectDto;
+import kafkademo.taskmanagersystem.repo.ProjectRepository;
+import kafkademo.taskmanagersystem.service.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProjectServiceImpl implements ProjectService {
+    private final ProjectRepository projectRepository;
+
+    @Override
+    public ProjectDto create(CreateProjectDto createProjectDto) {
+        return null;
+        //TODO: add logic
+    }
+
+    @Override
+    public List<ProjectDto> getByUser(Long userId) {
+        return List.of();
+        //TODO: add logic
+    }
+
+    @Override
+    public ProjectDto getById(Long id) {
+        return null;
+        //TODO: add logic
+    }
+
+    @Override
+    public ProjectDto updateById(Long id) {
+        return null;
+        //TODO: add logic
+    }
+
+    @Override
+    public ProjectDto deleteById(Long id) {
+        return null;
+        //TODO: add logic
+    }
 }
