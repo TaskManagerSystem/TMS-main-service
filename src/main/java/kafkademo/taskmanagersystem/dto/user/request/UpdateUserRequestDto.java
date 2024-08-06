f6db2cae3391ff20b2e3eb4f777d335119cc46bf
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-08-06 13:10:06 +0300

    feature: added user controller

--- src/main/java/kafkademo/taskmanagersystem/dto/user/request/UpdateUserRequestDto.java
+++ src/main/java/kafkademo/taskmanagersystem/dto/user/request/UpdateUserRequestDto.java
@@ -1,10 +1,14 @@
 package kafkademo.taskmanagersystem.dto.user.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class UpdateUserRequestDto {
+    @NotNull
     private String userName;
+    @NotNull
     private String userFirstName;
+    @NotNull
     private String userLastName;
 }
