3e281b985fd5115f6f67e67c534d203ee70ac48f
DmytroHadiuchko <153805566+DmytroHadiuchko@users.noreply.github.com>
2024-08-07 12:12:44 +0300

    Added password validation (#23)
    * feature: added password validation
    
    * feature: added password validation
    
    * fix: fixed checkstyle
    
    * fix dto and add dependence
    
    * fix properties
    
    ---------
    
    Co-authored-by: Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
    Co-authored-by: reamfox <sashafarion@gmail.com>

---
+++ src/main/java/kafkademo/taskmanagersystem/validation/impl/FieldMatchValidator.java
@@ -0,0 +1,30 @@
+package kafkademo.taskmanagersystem.validation.impl;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.util.Objects;
+import kafkademo.taskmanagersystem.validation.FieldMatch;
+import org.springframework.beans.BeanWrapperImpl;
+
+public class FieldMatchValidator implements ConstraintValidator<FieldMatch, Object> {
+
+    private String field;
+    private String fieldMatch;
+
+    @Override
+    public void initialize(FieldMatch constraintAnnotation) {
+        this.field = constraintAnnotation.field();
+        this.fieldMatch = constraintAnnotation.fieldMatch();
+    }
+
+    @Override
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        try {
+            Object fieldValue = new BeanWrapperImpl(value).getPropertyValue(field);
+            Object fieldMatchValue = new BeanWrapperImpl(value).getPropertyValue(fieldMatch);
+            return Objects.equals(fieldValue, fieldMatchValue);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
