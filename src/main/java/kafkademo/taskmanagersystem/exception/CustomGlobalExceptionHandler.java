17807fa707244ef13e8457374043e86193d090cf
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-08-07 12:17:15 +0300

    refactor : added exception to exception handler

--- src/main/java/kafkademo/taskmanagersystem/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/kafkademo/taskmanagersystem/exception/CustomGlobalExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -42,7 +43,8 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
 
     @ExceptionHandler({
             EntityNotFoundException.class,
-            InvalidStatusException.class
+            InvalidStatusException.class,
+            UsernameNotFoundException.class
     })
     public ResponseEntity<Object> handleNotFoundExceptions(
             Exception e) {
