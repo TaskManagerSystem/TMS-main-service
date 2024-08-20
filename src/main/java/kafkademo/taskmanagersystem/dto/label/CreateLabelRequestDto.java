f53344aed7ee5cc0297996669f65bee581ddcba1
reamfox <sashafarion@gmail.com>
2024-08-20 14:59:03 +0300

    add dto validation for create dtos

--- src/main/java/kafkademo/taskmanagersystem/dto/label/CreateLabelRequestDto.java
+++ src/main/java/kafkademo/taskmanagersystem/dto/label/CreateLabelRequestDto.java
@@ -1,9 +1,15 @@
 package kafkademo.taskmanagersystem.dto.label;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CreateLabelRequestDto {
+    @NotBlank
+    @Size(max = 30, message = "Label name can't be longer than 30 characters")
     private String name;
+    @NotBlank
+    @Size(max = 30, message = "Label color can't be longer than 30 characters")
     private String color;
 }
--- src/main/java/kafkademo/taskmanagersystem/dto/project/CreateProjectDto.java
+++ src/main/java/kafkademo/taskmanagersystem/dto/project/CreateProjectDto.java
@@ -1,14 +1,23 @@
 package kafkademo.taskmanagersystem.dto.project;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.Set;
 import lombok.Data;
 
 @Data
 public class CreateProjectDto {
+    @NotBlank
+    @Size(max = 40, message = "Name can't be longer than 40 characters")
     private String name;
+    @NotBlank
+    @Size(max = 255, message = "Description can't be longer than 255 characters")
     private String description;
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
     private Set<Long> userIds;
 }
