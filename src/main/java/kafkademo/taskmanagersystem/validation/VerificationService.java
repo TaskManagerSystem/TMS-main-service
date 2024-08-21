9b4379daac7f9c8b9059e57cf6a88a490e931d53
reamfox <sashafarion@gmail.com>
2024-08-21 13:39:29 +0300

    fix

--- src/main/java/kafkademo/taskmanagersystem/validation/VerificationService.java
+++ src/main/java/kafkademo/taskmanagersystem/validation/VerificationService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class VerificationService {
     private static final int EXPIRATION_PERIOD = 1;
-    private final ConcurrentHashMap<String, VerificationData> verificationMap;
+    private final ConcurrentHashMap<String, VerificationData> verificationMap = new ConcurrentHashMap<>();
     private final UserRepository userRepository;
 
     public void saveVerificationData(String token, VerificationData verificationData) {
