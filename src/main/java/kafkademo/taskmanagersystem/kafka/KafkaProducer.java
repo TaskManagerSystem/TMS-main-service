216d66c71bbe989bca64daa561127da61cbf7b00
Oleksandr Farion <sashafarion@gmail.com>
2024-08-23 14:46:43 +0300

    refactor kafka producer (#49)

--- src/main/java/kafkademo/taskmanagersystem/kafka/KafkaProducer.java
+++ src/main/java/kafkademo/taskmanagersystem/kafka/KafkaProducer.java
@@ -1,6 +1,7 @@
 package kafkademo.taskmanagersystem.kafka;
 
 import com.example.dto.IsVerificationDto;
+import com.example.dto.NotificationData;
 import com.example.dto.VerificationData;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,25 +16,34 @@ public class KafkaProducer {
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
     public void sendResponseToAttachmentService(IsVerificationDto dto) {
-        try {
-            ProducerRecord<String, Object> record = new ProducerRecord<>(
-                    "token-validation-response-topic", dto);
-            kafkaTemplate.send(record);
-            log.info("Record sending: {}", record);
-        } catch (Exception e) {
-            log.error("Error sending response: {}", dto, e);
-        }
+        sendMessageToTheTopic("token-validation-response-topic", dto);
     }
 
     public void sendVerificationData(String token, VerificationData verificationData) {
+        sendMessageToTheTopic("email-validation-response-topic", token, verificationData);
+    }
+
+    public void sendNotificationData(NotificationData notificationData) {
+        sendMessageToTheTopic("notification-topic", notificationData);
+    }
+
+    private <V> void sendMessageToTheTopic(String topic, String key, V value) {
         try {
-            ProducerRecord<String, Object> record =
-                    new ProducerRecord<>("email-validation-response-topic",
-                            token,
-                            verificationData);
+            ProducerRecord<String, Object> record = new ProducerRecord<>(topic, key, value);
             kafkaTemplate.send(record);
+            log.info("Record sent: {}:{}", record.key(), record.value());
         } catch (Exception e) {
-            log.error("Error sending response: {}", verificationData, e);
+            log.error("Error sending response: {}:{}", key, value, e);
+        }
+    }
+
+    private <V> void sendMessageToTheTopic(String topic, V value) {
+        try {
+            ProducerRecord<String, Object> record = new ProducerRecord<>(topic, value);
+            kafkaTemplate.send(record);
+            log.info("Record sent: {}", record.value());
+        } catch (Exception e) {
+            log.error("Error sending response: {}", value, e);
         }
     }
 }
