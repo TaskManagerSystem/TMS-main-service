acfb625cc567b6d7182bf45ef0e3f8c3f7a028ae
DmytroHadiuchko <153805566+DmytroHadiuchko@users.noreply.github.com>
2024-08-23 11:50:02 +0300

    Changed kafka config (#44)
    * refactor: changed kafka config
    
    * refactor: changed kafka config
    
    * refactor: changed methods names
    
    ---------
    
    Co-authored-by: Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>

--- src/main/java/kafkademo/taskmanagersystem/config/KafkaProducerConfig.java
+++ src/main/java/kafkademo/taskmanagersystem/config/KafkaProducerConfig.java
@@ -2,7 +2,6 @@ package kafkademo.taskmanagersystem.config;
 
 import java.util.HashMap;
 import java.util.Map;
-import kafkademo.taskmanagersystem.dto.user.VerificationData;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
@@ -20,31 +18,16 @@ public class KafkaProducerConfig {
     private String bootstrapServers;
 
     @Bean
-    public ProducerFactory<String, String> stringProducerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
-    }
-
-    @Bean
-    public KafkaTemplate<String, String> stringKafkaTemplate() {
-        return new KafkaTemplate<>(stringProducerFactory());
-    }
-
-    @Bean
-    public ProducerFactory<String, VerificationData> verificationDataProducerFactory() {
+    public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        configProps.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
-    public KafkaTemplate<String, VerificationData> verificationDataKafkaTemplate() {
-        return new KafkaTemplate<>(verificationDataProducerFactory());
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 }
